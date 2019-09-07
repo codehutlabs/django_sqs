dc0c77994b63e6be34bf225e74cd74f698630af0
Robert Kuzma <robert@balavec.com>
2019-09-07 07:43:22 +0200

    Update sqsrunner.py

--- djangosqs/apps/website/management/commands/sqsrunner.py
+++ djangosqs/apps/website/management/commands/sqsrunner.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from djangosqs.apps.website.sqs import Sqs
+from djangosqs.settings import MICRO_CONFIG
 
 
 class Command(BaseCommand):
@@ -9,7 +10,13 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         print("========================")
-        sqs = Sqs()
+        region_name = str(MICRO_CONFIG["REGION_NAME"])
+        queue_name = str(MICRO_CONFIG["STANDARD_QUEUE"])
+        dl_queue_name = str(MICRO_CONFIG["DL_QUEUE"])
+
+        sqs = Sqs(
+            region_name=region_name, queue_name=queue_name, dl_queue_name=dl_queue_name
+        )
 
         while True:
             sqs.process_queue()
